be1f43a0347747204834069c7e59745f9a344dfa
Mintxo <mintxosola@gmail.com>
2023-09-13 10:22:41 +0200

    Implementadas clases Usuario y Direccion

--- src/main/java/GSILabs/BModel/Direccion.java
+++ src/main/java/GSILabs/BModel/Direccion.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GSILabs.BModel;
 
 /**
@@ -9,5 +5,54 @@ package GSILabs.BModel;
  * @author 34636
  */
 public class Direccion {
+    /*El sistema almacena información de locales de ocio en España. Un local tienen un nombre y está
+en una localización concreta. (Localidad, Provincia, Calle y Número). No puede haber dos locales
+en la misma dirección. Los locales pueden almacenar una breve descripción de no más de 300
+caracteres.*/
+    public String localidad;
+    public String provincia;
+    public String calle;
+    public int numero;
+
+    public Direccion(String localidad, String provincia, String calle, int numero) {
+	this.localidad = localidad;
+	this.provincia = provincia;
+	this.calle = calle;
+	this.numero = numero;
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="getters and setters">
+    public String getLocalidad() {
+	return localidad;
+    }
     
+    public void setLocalidad(String localidad) {
+	this.localidad = localidad;
+    }
+    
+    public String getProvincia() {
+	return provincia;
+    }
+    
+    public void setProvincia(String provincia) {
+	this.provincia = provincia;
+    }
+    
+    public String getCalle() {
+	return calle;
+    }
+    
+    public void setCalle(String calle) {
+	this.calle = calle;
+    }
+    
+    public int getNumero() {
+	return numero;
+    }
+    
+    public void setNumero(int numero) {
+	this.numero = numero;
+    }
+//</editor-fold>
+  
 }
--- src/main/java/GSILabs/BModel/Usuario.java
+++ src/main/java/GSILabs/BModel/Usuario.java
@@ -1,14 +1,86 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GSILabs.BModel;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 /**
  *
  * @author 34636
  */
 public class Usuario {
-    
+    /*El sistema almacena información de usarios. Cada usuario tiene un nick único de al menos tres
+caracteres y una contraseña. Además, se debe almacenar la información de la fecha de nacimiento
+del mismo. No se permiten usuario de menos de 14 años. Cada usuario debe tener un perfil
+determinado, a escoger entre Propietario y Cliente.*/
+    private String nick;
+    private String contraseña;
+    private LocalDate fechaNacimiento;
+    private tipoUsuario tipo;
 
+    public enum tipoUsuario {
+        PROPIETARIO,
+        CLIENTE
+    }
+
+    public Usuario(String n, String c, LocalDate f, tipoUsuario t) {
+	nickValido(n); // this.nick = nick;
+	contraseñaValido(c); // this.contraseña = password;
+	fechaNacimientoValido(f); // this.fechaNacimiento = fechaNacimiento;
+	tipo = t;
+    }
+    
+    //<editor-fold defaultstate="collapsed" desc="getters and setters">
+    public String getNick() {
+	return nick;
+    }
+    
+    public void setNick(String nick) {
+	this.nick = nick;
+    }
+    
+    public String getContraseña() {
+	return contraseña;
+    }
+    
+    public void setContraseña(String contraseña) {
+	this.contraseña = contraseña;
+    }
+    
+    public LocalDate getFechaNacimiento() {
+	return fechaNacimiento;
+    }
+    
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+	this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    public tipoUsuario getTipo() {
+	return tipo;
+    }
+    
+    public void setTipo(tipoUsuario tipo) {
+	this.tipo = tipo;
+    }
+//</editor-fold>
+
+    public void nickValido(String n) {
+        if (this.nick.length() < 3) {
+            throw new IllegalArgumentException("El nombre debe tener al menos 3 caracteres."); // revisar esto que hace y sino solo sout
+        }
+        this.nick = n;
+    }
+    
+    public void contraseñaValido(String c) {
+        if (this.contraseña.length() < 1) {
+            throw new IllegalArgumentException("La contraseña no puede ser nula."); // revisar esto que hace y sino solo sout
+        }
+        this.contraseña = c;
+    }
+    
+    public void fechaNacimientoValido(LocalDate f) {
+        if (this.fechaNacimiento.getYear() > LocalDate.now().getYear()-14) {
+            throw new IllegalArgumentException("La contraseña no puede ser nula."); // revisar esto que hace y sino solo sout
+        }
+        this.fechaNacimiento = f;
+    }
 }
