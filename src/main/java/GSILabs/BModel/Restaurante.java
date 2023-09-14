63b3432561d2c43e44aa7feb526bfd03030a341d
Atracos <100346787+Atracos@users.noreply.github.com>
2023-09-14 20:03:12 +0200

    Add files via upload

--- src/main/java/GSILabs/BModel/Restaurante.java
+++ src/main/java/GSILabs/BModel/Restaurante.java
@@ -4,10 +4,47 @@
  */
 package GSILabs.BModel;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 34636
  */
 public class Restaurante extends Local implements Reservable{
-    
+    private double precioMenu;
+    private Integer capacidad;
+    private Integer capacidadMesa;
+    private ArrayList<Reserva> reservas;
+
+    public Restaurante(double precioMenu, Integer capacidad, Integer capacidadMesa){
+        this.precioMenu = precioMenu;
+        this.capacidad = capacidad;
+        this.capacidadMesa = capacidadMesa;
+        this.reservas = new ArrayList<>();
+
+    }
+
+    public double getPrecioMenu(){
+        return this.precioMenu;
+    }
+
+    public Integer capacidad(){
+        return this.capacidad;
+    }
+
+    public Integer capacidadMesa(){
+        return this.capacidadMesa;
+    }
+
+    public void setPrecioMenu(double precioMenu){
+        this.precioMenu = precioMenu;
+    }
+
+    public void setCapacidad(Integer capacidad){
+        this.capacidad=capacidad;
+    }
+
+    public void setCapacidadMesa(Integer capacidadMesa){
+        this.capacidadMesa=capacidadMesa;
+    }
 }
