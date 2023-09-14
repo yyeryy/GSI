4e7fb97ded8b14b6c21ca3a9d4f31b03bbaf879c
isusi.133488 <isusi.133488@e.unavarra.es>
2023-09-14 11:20:10 +0200

    Reservable completado

--- src/main/java/GSILabs/BModel/Reservable.java
+++ src/main/java/GSILabs/BModel/Reservable.java
@@ -4,10 +4,36 @@
  */
 package GSILabs.BModel;
 
+import java.util.ArrayList;
+import GSILabs.BModel.Reserva;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author 34636
  */
 public interface Reservable {
+/*
+    Tanto los restaurantes como los Bares son Reservables. El sistema debe poder almacenar Reservas
+de Clientes para Bares o Restaurantes. Cada Reserva incluye únicamente la fecha y hora en que
+se efectuará, así como un posible porcentaje de descuento.
+*/
+
+    ArrayList<ClienteReserva> listaReserva = new  ArrayList<ClienteReserva>();
+
+    public void nuevaReserva(Cliente c, LocalDate ld, LocalTime lt);
+
+    public Reserva[] reservasDeCliente(Cliente c);
+
+    public boolean estaReserva(Reserva r);
+
+    public boolean eliminarReserva(Reserva r);
+
+    public Reserva[] todasReserva();
     
+    public Reserva[] reservasDeDia(LocalDate ld);
+
 }
