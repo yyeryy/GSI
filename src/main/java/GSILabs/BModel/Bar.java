277bf399ce9c7cefe1cc684f3a06ca71fe344535
rulos01 <63600546+rulos01@users.noreply.github.com>
2023-09-15 19:39:00 +0200

    Update Bar.java

--- src/main/java/GSILabs/BModel/Bar.java
+++ src/main/java/GSILabs/BModel/Bar.java
@@ -9,5 +9,25 @@ package GSILabs.BModel;
  * @author 34636
  */
 public class Bar extends Local implements Reservable{
-    
+    private List<String> especialidades;
+
+    public Bar() {
+        this.especialidades = new ArrayList<>();
+    }
+
+    public void agregarEspecialidad(String especialidad) {
+        especialidades.add(especialidad);
+    }
+
+    public void mostrarEspecialidades() {
+        System.out.println("Especialidades de " + nombre + ":");
+        for (String especialidad : especialidades) {
+            System.out.println("- " + especialidad);
+        }
+    }
+
+    public static void main(String[] args) {
+        miBar.agregarEspecialidad("Cerveza artesanal");
+        miBar.agregarEspecialidad("Cócteles de autor");
+    }
 }
