814d7da2a0f583b47b3c95a9e6b1c74ff0ead758
Yeray Aller <aller.133416@e.unavarra.es>
2023-09-19 19:44:42 +0200

    S08

--- src/main/java/GSILabs/BTesting/P01/S08.java
+++ src/main/java/GSILabs/BTesting/P01/S08.java
@@ -1,10 +1,24 @@
 package GSILabs.BTesting.P01;
 
+import GSILabs.BModel.Bar;
+import GSILabs.BModel.Cliente;
+import GSILabs.BModel.Direccion;
+import GSILabs.BModel.Local;
+import static GSILabs.BModel.Local.tipoLocal.BAR;
+import GSILabs.BModel.Reservable;
+import static GSILabs.BModel.Usuario.tipoUsuario.CLIENTE;
+import GSILabs.BSystem.BusinessSystem;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * No se pueden añadir comentarios para Reviews que no existen;
  */
 public class S08 {
+    BusinessSystem bs = new BusinessSystem();
+    
     boolean testS8() {
+        bs.
         return false;
     }
 }
