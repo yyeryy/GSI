c32debb90cf7ac10828ec469750b60d5fd4a0ce1
Javier <jotaviervillava@gmail.com>
2023-09-19 20:15:57 +0200

    Añadir Test 9

--- src/main/java/GSILabs/BTesting/P01/S07.java
+++ src/main/java/GSILabs/BTesting/P01/S07.java
@@ -1,11 +1,30 @@
 package GSILabs.BTesting.P01;
 
+import GSILabs.BModel.Bar;
+import GSILabs.BModel.Cliente;
+import GSILabs.BModel.Direccion;
+import static GSILabs.BModel.Usuario.tipoUsuario.CLIENTE;
+import GSILabs.BSystem.BusinessSystem;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * No se pueden hacer reservas para un local inexistente, aunque esté en la misma dirección que
 otro existente;
  */
 public class S07 {
+    BusinessSystem bs = new BusinessSystem();
+    
     boolean testS7() {
-        return false;
+        // Creo al cliente
+        Cliente cliente = new Cliente("Prueba", "1234", LocalDate.of(2000,1,1),CLIENTE);
+        bs.nuevoUsuario(cliente);
+        // Creo el local
+        Direccion direccion = new Direccion("a","b","c",1);
+        Bar bar1 = new Bar("Bar1", direccion, "Bar1 para el ejemplo S07");
+        bs.nuevoLocal(bar1);
+        // Hacer reserva en un bar falso con la misma direccion
+        Bar bar2 = new Bar("Bar2", direccion, "Bar2 para el ejemplo S07");
+        return !bs.nuevaReserva(cliente, bar2, LocalDate.MIN, LocalTime.MIN);
     }
 }
--- src/main/java/GSILabs/BTesting/P01/S09.java
+++ src/main/java/GSILabs/BTesting/P01/S09.java
@@ -1,10 +1,36 @@
 package GSILabs.BTesting.P01;
 
+import GSILabs.BModel.Direccion;
+import GSILabs.BModel.Local;
+import static GSILabs.BModel.Local.tipoLocal.PUB;
+import GSILabs.BModel.Propietario;
+import GSILabs.BModel.Usuario;
+import static GSILabs.BModel.Usuario.tipoUsuario.PROPIETARIO;
+import GSILabs.BSystem.BusinessSystem;
+import java.time.LocalDate;
+
 /**
  * No se pueden añadir cuatro dueños a un bar;
  */
 public class S09 {
+    BusinessSystem bs = new BusinessSystem();
+    
     boolean testS9() {
+        // Creo los 4 dueños y los añado al sistema
+        for(int i = 0; i<4; i++)
+        {
+            Propietario propietario = new Propietario("dueno"+i+"","1234",LocalDate.of(2000,1,1),PROPIETARIO);
+            bs.nuevoUsuario(propietario);
+        }
+        // Creo un local para añadirle dueños
+        Direccion direccion = new Direccion("a","b","c",1);
+        Local local = new Local("Pruebas", direccion, "Pub de prueba", PUB);
+        // Añado los dueños
+        try{
+            for(int i = 0; i<4; i++)
+                bs.asociarLocal(local, (Propietario) bs.obtenerUsuario("dueno"+i+""));
+        }catch(IllegalArgumentException e){
+            return true;}
         return false;
     }
 }
