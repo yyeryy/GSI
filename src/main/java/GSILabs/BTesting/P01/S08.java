3359543f7a89f050747ed8085253614aa0ad3ebe
Yeray Aller <aller.133416@e.unavarra.es>
2023-09-19 20:00:39 +0200

    S08

--- src/main/java/GSILabs/BTesting/P01/S08.java
+++ src/main/java/GSILabs/BTesting/P01/S08.java
@@ -2,10 +2,13 @@ package GSILabs.BTesting.P01;
 
 import GSILabs.BModel.Bar;
 import GSILabs.BModel.Cliente;
+import GSILabs.BModel.Contestacion;
 import GSILabs.BModel.Direccion;
 import GSILabs.BModel.Local;
 import static GSILabs.BModel.Local.tipoLocal.BAR;
 import GSILabs.BModel.Reservable;
+import GSILabs.BModel.Review;
+import GSILabs.BModel.Usuario;
 import static GSILabs.BModel.Usuario.tipoUsuario.CLIENTE;
 import GSILabs.BSystem.BusinessSystem;
 import java.time.LocalDate;
@@ -18,7 +21,9 @@ public class S08 {
     BusinessSystem bs = new BusinessSystem();
     
     boolean testS8() {
-        
-        return false;
+        Usuario usuario = new Usuario("Prueba", "1234", LocalDate.of(2000,1,1), CLIENTE);
+        Local local = new Local("Casa pepe", new Direccion("Pamplona", "Navarra", "calle", 7), "el local", Local.tipoLocal.BAR);
+        Contestacion contestacion = new Contestacion("comentario", LocalDate.now(), local);
+        return !bs.nuevaContestacion(contestacion, new Review(3, "coment1", LocalDate.now(), local, usuario));
     }
 }
