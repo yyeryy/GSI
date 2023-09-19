6f922d22b2b1807ff987c57bf365ac4dd6a71162
Javier <jotaviervillava@gmail.com>
2023-09-19 19:03:47 +0200

    Añadir Test 6

--- src/main/java/GSILabs/BTesting/P01/S06.java
+++ src/main/java/GSILabs/BTesting/P01/S06.java
@@ -1,10 +1,27 @@
 package GSILabs.BTesting.P01;
 
+import GSILabs.BModel.Bar;
+import GSILabs.BModel.Cliente;
+import GSILabs.BModel.Direccion;
+import GSILabs.BModel.Local;
+import static GSILabs.BModel.Local.tipoLocal.BAR;
+import GSILabs.BModel.Reservable;
+import static GSILabs.BModel.Usuario.tipoUsuario.CLIENTE;
+import GSILabs.BSystem.BusinessSystem;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * No se pueden hacer reservas para un local inexistente;
  */
 public class S06 {
+    BusinessSystem bs = new BusinessSystem();
+    
     boolean testS6() {
-        return false;
+        Cliente cliente = new Cliente("Prueba", "1234", LocalDate.of(2000,1,1),CLIENTE);
+        bs.nuevoUsuario(cliente);
+        Direccion direccion = new Direccion("a","b","c",1);
+        Bar bar = new Bar("Bar", direccion, "Local para el ejemplo S06");
+        return !bs.nuevaReserva(cliente, bar, LocalDate.MIN, LocalTime.MIN);
     }
 }
