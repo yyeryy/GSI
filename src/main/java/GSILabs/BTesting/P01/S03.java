84a91390dc2460d5f36109ca6fa5ecb9388e9b60
Javier <jotaviervillava@gmail.com>
2023-09-19 18:17:34 +0200

    Creacion Test 3

--- src/main/java/GSILabs/BTesting/P01/S03.java
+++ src/main/java/GSILabs/BTesting/P01/S03.java
@@ -1,10 +1,23 @@
 package GSILabs.BTesting.P01;
 
+import GSILabs.BModel.Direccion;
+import GSILabs.BModel.Local;
+import GSILabs.BModel.Direccion;
+import static GSILabs.BModel.Local.tipoLocal.*;
+import GSILabs.BSystem.BusinessSystem;
+
 /**
  * No se puede introducir los Locales en la misma dirección;
  */
 public class S03 {
+    BusinessSystem bs = new BusinessSystem();
+    
+    
     boolean testS3() {
-        return false;
+        Direccion direccion = new Direccion("a","b","c",1);
+        Local local1 = new Local("Local1", direccion, "Local 1 para el ejemplo S03", BAR);
+        Local local2 = new Local("Local2", direccion, "Local 2 para el ejemplo S03", RESTAURANTE);   
+        bs.nuevoLocal(local1);
+        return !bs.nuevoLocal(local2);
     }
 }
