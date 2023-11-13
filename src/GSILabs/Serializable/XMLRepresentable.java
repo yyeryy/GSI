837589205a30ffb371933448957b6e453d67fd86
Julen Huarte <huarte.133745@e.unavarra.es>
2023-11-13 21:49:21 +0100

    Generación de javadoc de XMLRepresentable

--- src/GSILabs/Serializable/XMLRepresentable.java
+++ src/GSILabs/Serializable/XMLRepresentable.java
@@ -11,8 +11,9 @@ package GSILabs.serializable;
 import java.io.File;
 
 /**
+ * XMLRepresentable
  * This interface embodies the ability of a a class of objects to represent itself
- * 	using XML. 
+ * using XML. 
  * Note that it refers, in no manner, to its ability to create instances from such a representation.
  *
  * @author carlos.lopez
@@ -20,10 +21,27 @@ import java.io.File;
  */
 public interface XMLRepresentable {
     
+    /**
+     * Generación de una representación XML del objeto
+     * que implementa la interfaz.
+     * @return Representación XML del objeto en forma de cadena
+     */
     public String toXML();
     
+    /**
+     * Guardado de la representación XML del objeto que implementa la interfaz
+     * en el fichero indicado por parámetro.
+     * @param f Fichero XML en el que se guarda la representación XML del objeto
+     * @return Booleano que indica si el fichero se ha guardado exitosamente.
+     */
     public boolean saveToXML(File f);
     
+    /**
+     * Guardado de la representación XML del objeto que implementa la interfaz
+     * en un fichero XML que se almacenará en la ruta indicada por parámetro.
+     * @param filePath Ruta del fichero donde se va a guardar la reprentación XML.
+     * @return Booleano que indica si el fichero se ha guardado exitosamente.
+     */
     public boolean saveToXML(String filePath);
     
 }
