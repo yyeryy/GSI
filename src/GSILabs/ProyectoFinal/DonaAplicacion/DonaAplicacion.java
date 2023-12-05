dfa36a2d41e76cfaa60bff5853e3cf121fd0c9a7
Julen Huarte <huarte.133745@e.unavarra.es>
2023-12-05 13:34:21 +0100

    Estructuración de Carpetas

---
+++ src/GSILabs/ProyectoFinal/DonaAplicacion/DonaAplicacion.java
@@ -0,0 +1,18 @@
+package GSILabs.ProyectoFinal.DonaAplicacion;
+
+/**
+ * Clase DonaAplicacion
+ * Clase con la que ejecutaremos el proyecto el menú principal del proyecto
+ * final para empezar con la ejecución del Proyecto Final con el nombre comercial
+ * DonaAplicacion.
+ * @author Grupo 3 - GSI
+ * @version 1.0
+ * @since 02.12.2023
+ */
+public class DonaAplicacion {
+
+    public static void main(String[] args) {
+        new MenuPrincipal();
+    }
+    
+}
