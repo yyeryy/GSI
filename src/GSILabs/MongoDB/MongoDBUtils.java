16bfe9017d4ce4d0bf3d5839dce33a845764010d
Javier <aranguren.126084@e.unavarra.es>
2023-12-08 18:06:10 +0100

    Javadoc de MongoDBUtils

--- src/GSILabs/MongoDB/MongoDBUtils.java
+++ src/GSILabs/MongoDB/MongoDBUtils.java
@@ -15,7 +15,12 @@ import static org.apache.commons.collections.CollectionUtils.size;
 import org.bson.Document;
 
 public class MongoDBUtils {
-
+    /**
+     * Funcion crearDireccionDocument
+     * Genera un Document a partir de Direccion.
+     * @param direccion: Direccion a convertir.
+     * @return Document: Document obtenido de Direccion.
+     */
     public static Document crearDireccionDocument(Direccion direccion) {
         Document direccionDocument = new Document();
         direccionDocument.append("Localidad", direccion.getLocalidad());
@@ -25,6 +30,12 @@ public class MongoDBUtils {
         return direccionDocument;
     }
     
+    /**
+     * Funcion crearDireccionObject
+     * Genera una Direccion a partir de Document.
+     * @param direccionDocument: Document a convertir.
+     * @return Direccion: Direccion obtenida de Document.
+     */
     public static Direccion crearDireccionObject(Document direccionDocument) {
         String localidad = direccionDocument.getString("Localidad");
         String provincia = direccionDocument.getString("Provincia");
@@ -33,6 +44,12 @@ public class MongoDBUtils {
         return new Direccion(localidad,provincia,calle,numero);
     }
     
+    /**
+     * Funcion crearUsuarioDocument
+     * Genera un Document a partir de Direccion.
+     * @param usuario: Direccion a convertir.
+     * @return Document: Document obtenido de Usuario.
+     */
     public static Document crearUsuarioDocument(Usuario usuario) {
         Document usuarioDocument = new Document();
         usuarioDocument.append("Nick", usuario.getNick());
@@ -42,6 +59,12 @@ public class MongoDBUtils {
         return usuarioDocument;
     }
     
+    /**
+     * Funcion crearUsuarioObject
+     * Genera una Usuario a partir de Document.
+     * @param usuarioDocument: Document a convertir.
+     * @return Usuario: Usuario obtenida de Document.
+     */
     public static Usuario crearUsuarioObject(Document usuarioDocument) {
         String nick = usuarioDocument.getString("Nick");
         String contrasena = usuarioDocument.getString("Contraseña");
@@ -50,6 +73,12 @@ public class MongoDBUtils {
         return new Usuario(nick, contrasena, fechaNacimiento, tipo);
     }
     
+    /**
+     * Funcion crearPropietarioDocument
+     * Genera un Document a partir de Propietario.
+     * @param propietario: Propietario a convertir.
+     * @return Document: Document obtenido de Propietario.
+     */
     public static Document crearPropietarioDocument(Propietario propietario) {
         Document propietarioDocument = new Document();
         propietarioDocument.append("Nick", propietario.getNick());
@@ -58,6 +87,12 @@ public class MongoDBUtils {
         return propietarioDocument;
     }
     
+    /**
+     * Funcion crearPropietarioObject
+     * Genera un Propietario a partir de Document.
+     * @param propietarioDocument: Document a convertir.
+     * @return Propietario: Propietario obtenido de Document.
+     */
     public static Propietario crearPropietarioObject(Document propietarioDocument) {
         String nick = propietarioDocument.getString("Nick");
         String contrasena = propietarioDocument.getString("Contraseña");
@@ -65,6 +100,12 @@ public class MongoDBUtils {
         return new Propietario(nick, contrasena, fechaNacimiento);
     }
 
+    /**
+     * Funcion crearClienteDocument
+     * Genera un Document a partir de Cliente.
+     * @param cliente: Cliente a convertir.
+     * @return Document: Document obtenido de Cliente.
+     */
     public static Document crearClienteDocument(Cliente cliente) {
         Document clienteDocument = new Document();
         clienteDocument.append("Nick", cliente.getNick());
@@ -73,6 +114,12 @@ public class MongoDBUtils {
         return clienteDocument;
     }
     
+    /**
+     * Funcion crearClienteObject
+     * Genera un Cliente a partir de Document.
+     * @param clienteDocument: Document a convertir.
+     * @return Cliente: Cliente obtenido de Document.
+     */
     public static Cliente crearClienteObject(Document clienteDocument) {
         String nick = clienteDocument.getString("Nick");
         String contrasena = clienteDocument.getString("Contraseña");
@@ -80,6 +127,12 @@ public class MongoDBUtils {
         return new Cliente(nick, contrasena, fechaNacimiento);
     }
     
+    /**
+     * Funcion crearLocalDocument
+     * Genera un Document a partir de Local.
+     * @param local: Cliente a convertir.
+     * @return Document: Document obtenido del Local.
+     */
     public static Document crearLocalDocument(Local local){
         Document localDocument = new Document();
         localDocument.append("Nombre", local.getNombre());
@@ -95,6 +148,12 @@ public class MongoDBUtils {
         return localDocument;
     }
     
+    /**
+     * Funcion crearLocalObject
+     * Genera un Local a partir de un Document.
+     * @param localDocument: Document a convertir.
+     * @return Local: Local obtenido del Document.
+     */
     public static Local crearLocalObject(Document localDocument){
         String nombre = localDocument.getString("Nombre");
         Direccion direccion = crearDireccionObject(localDocument.get("Direccion", Document.class));
@@ -114,6 +173,12 @@ public class MongoDBUtils {
         return local;
     }
     
+    /**
+     * Funcion crearContestacionDocument
+     * Genera un Document a partir de Contestacion.
+     * @param contestacion: Contestacion a convertir.
+     * @return Document: Document obtenido de Contestacion.
+     */
     public static Document crearContestacionDocument(Contestacion contestacion){
         Document contestacionDocument = new Document();
         contestacionDocument.append("Comentario", contestacion.getComentario());
@@ -122,6 +187,12 @@ public class MongoDBUtils {
         return contestacionDocument;
     }
     
+    /**
+     * Funcion crearContestacionObject
+     * Genera un Contestacion a partir de un Document.
+     * @param contestacionDocument: Document a convertir.
+     * @return Contestacion: Contestacion obtenido del Document.
+     */
     public static Contestacion crearContestacionObject(Document contestacionDocument){
         String comentario = contestacionDocument.getString("Comentario");
         LocalDate fechaReview = LocalDate.parse(contestacionDocument.getString("Fecha review"));
@@ -129,6 +200,12 @@ public class MongoDBUtils {
         return new Contestacion(comentario,fechaReview,local);
     }
     
+    /**
+     * Funcion crearReviewDocument
+     * Genera un Document a partir de Review.
+     * @param review: Review a convertir.
+     * @return Document: Document obtenido de Review.
+     */
     public static Document crearReviewDocument(Review review){
         Document reviewDocument = new Document();
         reviewDocument.append("Valoracion", review.getValoracion());
@@ -141,6 +218,12 @@ public class MongoDBUtils {
         return reviewDocument;
     }
     
+    /**
+     * Funcion crearReviewObject
+     * Genera un Review a partir de un Document.
+     * @param reviewDocument: Document a convertir.
+     * @return Review: review obtenido del Document.
+     */
     public static Review crearReviewObject(Document reviewDocument){
         int valoracion = reviewDocument.getInteger("Valoracion");
         String comentario = reviewDocument.getString("Comentario");
