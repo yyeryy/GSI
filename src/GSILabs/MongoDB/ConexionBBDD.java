c1cb67cb6527e5a8213dcb347c0223d531284fda
Javier <aranguren.126084@e.unavarra.es>
2023-12-08 16:37:34 +0100

    Cambio MongoDB, la lista de reviews es visible y se ha creado una clase que simplifica crear Documents (Los usa MongoDB)

--- src/GSILabs/MongoDB/ConexionBBDD.java
+++ src/GSILabs/MongoDB/ConexionBBDD.java
@@ -8,6 +8,14 @@ import GSILabs.BModel.Review;
 import GSILabs.BModel.Usuario;
 import GSILabs.BModel.Usuario.tipoUsuario;
 import GSILabs.BSystem.BusinessSystem;
+import static GSILabs.MongoDB.MongoDBUtils.crearDireccionDocument;
+import static GSILabs.MongoDB.MongoDBUtils.crearDireccionObject;
+import static GSILabs.MongoDB.MongoDBUtils.crearLocalDocument;
+import static GSILabs.MongoDB.MongoDBUtils.crearLocalObject;
+import static GSILabs.MongoDB.MongoDBUtils.crearReviewDocument;
+import static GSILabs.MongoDB.MongoDBUtils.crearReviewObject;
+import static GSILabs.MongoDB.MongoDBUtils.crearUsuarioDocument;
+import static GSILabs.MongoDB.MongoDBUtils.crearUsuarioObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
@@ -18,7 +26,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import static org.apache.commons.collections.CollectionUtils.size;
@@ -41,92 +48,47 @@ public class ConexionBBDD {
         MongoClient mongoClient = MongoDBSingleton.getMongoClient();
         MongoDatabase database = MongoDBSingleton.getDatabase();
         // Subir el contenido de Business System
-        // LOCALES
+        // LOCAL
         try{
             // Borro lo viejo, sino se duplicara
             database.getCollection("Locales").drop();
             MongoCollection<Document> localesCollection = database.getCollection("Locales");
             for(Local local : bs.locales){
-                // Direccion
-                Document direccionDocument = new Document();
-                direccionDocument.append("Localidad", local.getDireccion().getLocalidad());
-                direccionDocument.append("Provincia", local.getDireccion().getProvincia());
-                direccionDocument.append("Calle", local.getDireccion().getCalle());
-                direccionDocument.append("Numero", local.getDireccion().getNumero());
-                // Local
-                Document localDocument = new Document();
-                localDocument.append("Nombre", local.getNombre());
-                localDocument.append("Direccion", direccionDocument);
-                localDocument.append("Descripcion", local.getDescripcion());
-                localDocument.append("Tipo local", local.getTipo().toString());
-                // Propietarios
-                int i = 1;
-                for(Propietario propietario:local.getPropietarios()){
-                    Document propietarioDocument = new Document();
-                    propietarioDocument.append("Nick", propietario.getNick());
-                    propietarioDocument.append("Contraseña", propietario.getContraseña());
-                    propietarioDocument.append("Fecha nacimiento", propietario.getFechaNacimiento().toString());
-                    propietarioDocument.append("Tipo usuario", propietario.getTipo().name());
-                    localDocument.append("Propietario " + i, propietarioDocument);
-                    i++;
-                }  
+                Document localDocument = crearLocalDocument(local);
                 localesCollection.insertOne(localDocument);
             }
-        }/*
-        try{
-            // Borro lo viejo, sino se duplicara
-            database.getCollection("Locales").drop();
-            MongoCollection<Document> localesCollection = database.getCollection("Locales");
-           
-            for(Local local : bs.locales){
-                byte[] serializedLocal = serializeObject(local);
-                Document document = new Document("serializedLocal",bytesToBinary(serializedLocal));
-                localesCollection.insertOne(document);
-            }
-        }*/
+        }
         catch(Exception e){
             System.out.println("ERROR: No se ha podido subir la lista de Locales");
             return false;
         }
+        
         // REVIEWS
         try{
             // Borro lo viejo, sino se duplicara
             database.getCollection("Reviews").drop();
             MongoCollection<Document> reviewsCollection = database.getCollection("Reviews");
             for(Review review : bs.reviews){
-                byte[] serializedReview = serializeObject(review);
-                Document document = new Document("serializedReview",bytesToBinary(serializedReview));
-                reviewsCollection.insertOne(document);
+                Document reviewDocument = crearReviewDocument(review);
+                reviewsCollection.insertOne(reviewDocument);
             }
         }
         catch(Exception e){
             System.out.println("ERROR: No se ha podido subir la lista de Reviews");
+            e.printStackTrace();
             return false;
         }
+        
         // USUARIOS
         try{
             // Borro lo viejo, sino se duplicara
             database.getCollection("Usuarios").drop();
             MongoCollection<Document> usuariosCollection = database.getCollection("Usuarios");
             for(Usuario usuario : bs.usuarios){
-                Document usuarioDocument = new Document();
-                usuarioDocument.append("Nick", usuario.getNick());
-                usuarioDocument.append("Contraseña", usuario.getContraseña());
-                usuarioDocument.append("Fecha nacimiento", usuario.getFechaNacimiento().toString());
-                usuarioDocument.append("Tipo usuario", usuario.getTipo().name());
+                Document usuarioDocument = crearUsuarioDocument(usuario);
                 usuariosCollection.insertOne(usuarioDocument);
             }
         }
-        /*try{
-            // Borro lo viejo, sino se duplicara
-            database.getCollection("Usuarios").drop();
-            MongoCollection<Document> usuariosCollection = database.getCollection("Usuarios");
-            for(Usuario usuario : bs.usuarios){
-                byte[] serializedUsuario = serializeObject(usuario);
-                Document document = new Document("serializedUsuario",bytesToBinary(serializedUsuario));
-                usuariosCollection.insertOne(document);
-            }
-        }*/
         catch(Exception e){
             System.out.println("ERROR: No se ha podido subir la lista de Usuarios");
             return false;
@@ -147,65 +109,28 @@ public class ConexionBBDD {
         // Crear BusinnesSytem
         BusinessSystem bs = new BusinessSystem();
         // Obtener los datos de la base de datos
-        // LOCALES
+        
+// LOCALES
         List<Local> locales = new ArrayList<>();
         try{
             MongoCollection<Document> localesCollection = database.getCollection("Locales");
             FindIterable<Document> iterable = localesCollection.find();
             for(Document localDocument: iterable){
-                // Direccion
-                Document direccionDocument = (Document) localDocument.get("Direccion");
-                String localidad = direccionDocument.getString("Localidad");
-                String provincia = direccionDocument.getString("Provincia");
-                String calle = direccionDocument.getString("Calle");
-                Integer numero = direccionDocument.getInteger("Numero");
-                Direccion direccion = new Direccion(localidad,provincia,calle,numero);
-                // Propietarios
-                List<Propietario> propietarios = new ArrayList<>();
-                for(int i = 1; i <= 3; i++)
-                {
-                    try{
-                        Document propietarioDocument = (Document) localDocument.get("Propietario " + i);
-                        String nick = propietarioDocument.getString("Nick");
-                        String contrasena = propietarioDocument.getString("Contraseña");
-                        LocalDate fechaNacimiento = LocalDate.parse(propietarioDocument.getString("Fecha nacimiento"));
-                        tipoUsuario tipo = tipoUsuario.parse(propietarioDocument.getString("Tipo usuario"));
-                        Propietario propietario = new Propietario(nick,contrasena,fechaNacimiento);
-                        propietarios.add(propietario);
-                    }
-                    catch(Exception e){}
-                }
-                // Local
-                String nombre = localDocument.getString("Nombre");
-                String descripcion = localDocument.getString("Descripcion");
-                tipoLocal tipo = tipoLocal.parse(localDocument.getString("Tipo local"));
-                Local local = new Local(nombre,direccion,descripcion,tipo,propietarios.get(0));
-                for(int i = 1; i < size(propietarios); i++)
-                    local.addPropietario(propietarios.get(i));
-            }
-        }
-        /*
-        try{
-            MongoCollection<Document> localesCollection = database.getCollection("Locales");
-            FindIterable<Document> iterable = localesCollection.find();
-            for(Document document : iterable){
-                byte[] serializedLocal = binaryToBytes((Binary)document.get("serializedLocal"));
-                Local local = deserializeObject(serializedLocal,Local.class);
+                Local local = crearLocalObject(localDocument);
                 locales.add(local);
             }
-        }*/
+        }
         catch(Exception e){
             System.out.println("ERROR: No se ha podido obtener la lista de Locales");
             return null;
         }
-        // Review
+        // REVIEW
         List<Review> reviews = new ArrayList<>();
         try{
             MongoCollection<Document> reviewsCollection = database.getCollection("Reviews");
             FindIterable<Document> iterable = reviewsCollection.find();
-            for(Document document : iterable){
-                byte[] serializedReview = binaryToBytes((Binary)document.get("serializedReview"));
-                Review review = deserializeObject(serializedReview,Review.class);
+            for(Document reviewDocument : iterable){
+                Review review = crearReviewObject(reviewDocument);
                 reviews.add(review);
             }
         }
@@ -213,30 +138,17 @@ public class ConexionBBDD {
             System.out.println("ERROR: No se ha podido obtener la lista de Reviews");
             return null;
         }
-        // Usuarios
+        
+        // USUARIOS
         List<Usuario> usuarios = new ArrayList<>();
         try{
             MongoCollection<Document> usuariosCollection = database.getCollection("Usuarios");
             FindIterable<Document> iterable = usuariosCollection.find();
             for(Document usuarioDocument : iterable){
-                String nick = usuarioDocument.getString("Nick");
-                String contrasena = usuarioDocument.getString("Contraseña");
-                LocalDate fechaNacimiento = LocalDate.parse(usuarioDocument.getString("Fecha nacimiento"));
-                tipoUsuario tipo = tipoUsuario.parse(usuarioDocument.getString("Tipo usuario"));
-                Usuario usuario = new Usuario(nick,contrasena,fechaNacimiento,tipo);
+                Usuario usuario = crearUsuarioObject(usuarioDocument);
                 usuarios.add(usuario);
             }
         }
-        /*
-        try{
-            MongoCollection<Document> usuariosCollection = database.getCollection("Usuarios");
-            FindIterable<Document> iterable = usuariosCollection.find();
-            for(Document document : iterable){
-                byte[] serializedUsuarios = binaryToBytes((Binary)document.get("serializedUsuario"));
-                Usuario usuario = deserializeObject(serializedUsuarios, Usuario.class);
-                usuarios.add(usuario);
-            }
-        }*/
         catch(Exception e){
             System.out.println("ERROR: No se ha podido obtener la lista de Usuarios");
             e.printStackTrace();
